90f1ff7f6bad1e2159da3110e1aaf1be21fd3b35
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-06-28 18:28:12 -0500

    added devDependencies

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 // Express
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
